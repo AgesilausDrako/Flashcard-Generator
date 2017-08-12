c96889d73131e59bb99fe0d907431277107a342e
AgesilausDrako <agesilaus@yahoo.com>
2017-08-12 17:29:40 -0400

    added dynamic card creation

--- flashcards.js
+++ flashcards.js
@@ -1,17 +1,62 @@
 // Variable for inquirer dependency
 var inquirer = require("inquirer");
+var fs = require("fs");
 
 // Variables for external constructor functions
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
 
-// Variables for external files with questions
-var clozeEntries = require("./clozeEntries").questions;
-var basicEntries = require("./basicEntries").questions;
+// Basic arrays with preexisting questions
+var initialBasicArray = [
+    {
+        front: "Who was the last tsar of Russia?",
+        back: "Nicholas II"
+    },
+    {
+        front: "Who was the first Soviet leader?",
+        back: "Vladimir Lenin"
+    },
+    {
+        front: "Who was the tsar who freed the serfs?",
+        back: "Alexander I"
+    },
+    {
+        front: "Who was the tsar of Russia during the Crimean War?",
+        back: "Nicholas I"
+    },
+    {
+        front: "Who was known as tsar known as 'the Terrible'?",
+        back: "Ivan IV"
+    }
+]; 
 
+// Cloze arrays with preexisting questions
+var initialClozeArray = [
+    {
+        text: "Nicholas II was the last tsar of Russia.",
+        cloze: "Nicholas II"
+    },
+    {
+        text: "Vladimir Lenin was the first Soviet leader.",
+        cloze: "Vladimir Lenin"
+    },
+    {
+        text: "Alexander I was the tsar who freed the serfs.",
+        cloze: "Alexander I"
+    },
+    {
+        text: "Nicholas I was the tsar of Russia during the Crimean War.",
+        cloze: "Nicholas I"
+    },
+    {
+        text: "Ivan IV was known as tsar known as 'the Terrible'",
+        cloze: "Ivan IV"
+    }
+]; 
 // Variables for arrays to hold the constructed objects
 var basicQuestionsArray = [];
 var clozeQuestionsArray = [];
+
 
 // Variable to track the question count, correct, and incorrect guesses
 var questionCount = 0;
@@ -25,23 +70,29 @@ function gameInit () {
             type: "list",
             name: "quizzes",
             message: "Do you want to play Basic Cards or Cloze Cards?",
-            choices: ["Basic Cards", "Cloze Cards"]
+            choices: ["Create Basic Card", "Create Cloze Card", "Play Basic Cards", "Play Cloze Cards"]
         }
     ]).then(function(answers) {
-
+        // If card creation chosen the appropriate create function is called
+        if (answers.quizzes === "Create Basic Card") {
+            createBasicEntry();
+        } else if (answers.quizzes === "Create Cloze Card") {
+            createClozeEntry();
         // If the user chooses Basic Cards the loop iterates through the basicEntries file 
         //creating constructed objects
-        if (answers.quizzes === "Basic Cards") {
-            for (var i = 0; i < basicEntries.length; i++) {
-                var createdBasicCards = new BasicCard(basicEntries[i].front, basicEntries[i].back);
+        } else if (answers.quizzes === "Play Basic Cards") {
+        
+            for (var i = 0; i < initialBasicArray.length; i++) {
+                var createdBasicCards = new BasicCard(initialBasicArray[i].front, initialBasicArray[i].back);
                 basicQuestionsArray.push(createdBasicCards);
             }
             // Call the function to run the basic questions
             basicQuestion();
-        } else {
+        } else if (answers.quizzes === "Play Cloze Cards"){
+
             // The loop iterates through the clozeEntries file creating constructed objects
-            for (var j = 0; j < clozeEntries.length; j++) {
-                var createdClozeCards = new ClozeCard(clozeEntries[j].text, clozeEntries[j].cloze);
+            for (var j = 0; j < initialClozeArray.length; j++) {
+                var createdClozeCards = new ClozeCard(initialClozeArray[j].text, initialClozeArray[j].cloze);
                 clozeQuestionsArray.push(createdClozeCards);
             }
             // Call the function to run the cloze questions
@@ -49,7 +100,6 @@ function gameInit () {
         }
     });
 }
-
 
 // Shows the question part of the card and prompts an answer
 function basicQuestion() {
@@ -118,6 +168,82 @@ function clozeQuestion() {
                 console.log("Correct: " + correct);
                 console.log("Incorrect: " + incorrect);
             }
+    });
+}
+
+// Creates the new array entry
+function createBasicEntry() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your basic question?",
+            name: "front",
+            validate: function(value) {
+                if (!value) {
+                  return false;
+              }
+              return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is your basic answer?",
+            name: "back",
+            validate: function(value) {
+                if (!value) {
+                  return false;
+              }
+              return true;
+            }
+        }
+    ]).then(function(answers) {
+        // Forms the object to be pushed to the array
+        var basicObject = {
+            front: answers.front,
+            back: answers.back
+        };
+        initialBasicArray.push(basicObject);
+
+        // Recalls the game menu
+        gameInit();
+    });
+}
+
+// Creates the new array entry
+function createClozeEntry() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your cloze statement?",
+            name: "text",
+            validate: function(value) {
+                if (!value) {
+                  return false;
+              }
+              return true;
+            }
+        },
+        {
+            type: "input",
+            message: "What is the cloze part?",
+            name: "cloze",
+            validate: function(value) {
+                if (!value) {
+                  return false;
+              }
+              return true;
+            }
+        }
+    ]).then(function(answers) {
+        // Forms the object to be pushed to the array
+        var clozeObject = {
+            text: answers.text,
+            cloze: answers.cloze
+        };
+        initialClozeArray.push(clozeObject);
+        
+        // Recalls the game menu
+        gameInit();
     });
 }
 
